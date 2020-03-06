b4805c3538df6d2fe872272bc0bba6c9b35fd6cc
Miraclx <omiraculous@gmail.com>
2020-03-06 07:48:54 +0100

    Add storefront cli flag (unimplemented)

--- cli.js
+++ cli.js
@@ -607,6 +607,7 @@ const command = commander
   .option('-f, --force', 'force overwrite of existing files')
   .option('-p, --playlist <file>', 'create playlist for all successfully collated tracks')
   .option('-P, --no-playlist', 'skip creating a playlist file for collections')
+  .option('-s, --storefront <COUNTRY>', 'country storefront code (unimplemented)', 'US')
   .option('-f, --filter <SEQ>', 'filter matches (unimplemented)')
   .option('-g, --groups <GROUP_TYPE>', 'filter collections by single/album/appears_on/compilation (unimplemented)')
   .option('-T, --no-tree', "don't organise tracks in format [PREFIX/]<ARTIST>/<ALBUM>/<TRACK>")
