4b068e0ef38e2a0bb1df4781273dbe843447601b
Miraclx <omiraculous@gmail.com>
2020-04-07 02:54:27 +0100

    Add support for handling errors with a function

--- cli.js
+++ cli.js
@@ -117,7 +117,11 @@ async function processPromise(px, logger, {pre, post, err, xerr} = {}) {
   if (pre) logger.print(pre);
   const rex = await Promise.resolve(typeof px === 'function' ? px() : px).reflect();
   if (rex.isRejected())
-    logger.write(...(err ? [err, '\n'] : [`(failed%s)\n`, (_err => (_err ? `: [${_err.message || _err}]` : ''))(rex.reason())]));
+    logger.write(
+      ...(err
+        ? [typeof err === 'function' ? err(rex.reason()) : err, '\n']
+        : [`(failed%s)\n`, (_err => (_err ? `: [${_err.message || _err}]` : ''))(rex.reason())]),
+    );
   else if (xerr && (!rex.value() || rex.value().err)) logger.write(`${xerr}\n`);
   else logger.write(`${post || '[done]'}\n`);
   return rex.isFulfilled() ? rex.value() : null;
