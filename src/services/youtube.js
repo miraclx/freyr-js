d27384947dd46e6294d56426fa9f0c18f9e14182
Miraclx <omiraculous@gmail.com>
2020-06-19 05:40:43 +0100

    Check item has a valid videoId before processing

--- src/services/youtube.js
+++ src/services/youtube.js
@@ -251,7 +251,7 @@ class YouTubeMusic {
     const classified = Object.values(
       validSections.reduce((final, item) => {
         // prune duplicates
-        if (!(item.link.videoId in final)) {
+        if (item && item.link.videoId && !(item.link.videoId in final)) {
           final[item.link.videoId] = {
             title: item.title,
             type: item.type,
@@ -357,7 +357,7 @@ class YouTube {
     }
     const classified = Object.values(
       searchResults.reduce((final, item) => {
-        if (!(item.videoId in final))
+        if (item.videoId && !(item.videoId in final))
           final[item.videoId] = {
             title: item.title,
             type: item.type,
