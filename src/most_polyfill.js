18b0fcdfa14d797a8b8b59e6c11dacb9faa62a47
Miraclx <omiraculous@gmail.com>
2020-06-21 10:43:35 +0100

    Update most polyfill to be non-exhaustive of the input array

--- src/most_polyfill.js
+++ src/most_polyfill.js
@@ -6,9 +6,10 @@ module.exports = function most(arr, fun) {
   const extent = Math.round(len / 2);
   let count = 0;
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const [i, v] of Object.entries(arr)) {
-    if (fun.call(null, v, i, arr)) count += 1;
+  // non-exhaustive iteration: check enough items to qualify this call
+  for (let index = 0; index < len; index += 1) {
+    const item = arr[index];
+    if (fun.call(null, item, index, arr)) count += 1;
     if (count === extent) return true;
   }
 
