2a03759877a8cefdfbcac7a8afccc1fea46dc2aa
Miraclx <omiraculous@gmail.com>
2021-01-04 06:29:10 +0100

    interop[py]: add proxies for more ytmusicapi methods

--- src/pyinterop/main.py
+++ src/pyinterop/main.py
@@ -33,8 +33,29 @@ class YouTubeMusic:
             self.__core = YTMusic()
         return self.__core
 
-    def search(self, *args):
-        return self._getCore().search(*args)
+    def search(self, query, *args):
+        return self._getCore().search(query, *args)
+
+    def get_artist(self, channelId):
+        return self._getCore().get_artist(channelId)
+
+    def get_artist_albums(self, channelId, params):
+        return self._getCore().get_artist_albums(channelId, params)
+
+    def get_album(self, browseId):
+        return self._getCore().get_album(browseId)
+
+    def get_song(self, videoId):
+        return self._getCore().get_song(videoId)
+
+    def get_lyrics(self, browseId):
+        return self._getCore().get_lyrics(browseId)
+
+    def get_watch_playlist(self, videoId, *args):
+        return self._getCore().get_watch_playlist(videoId, *args)
+
+    def get_playlist(self, playlistId, *args):
+        return self._getCore().get_playlist(playlistId, *args)
 
 
 handlers = {
