e4aff77eb052532ce42650c48ad5e9dd5be124a6
Maxence G <maxence@powerz.tech>
2021-10-01 23:07:57 +0200

    Final code cleaning and separation

--- cli.js
+++ cli.js
@@ -31,6 +31,7 @@ const symbols = require('./src/symbols');
 const fileMgr = require('./src/file_mgr');
 const pFlatten = require('./src/p_flatten');
 const FreyrCore = require('./src/freyr');
+const MusicBrainz = require('./src/musicbrainz.js');
 const AuthServer = require('./src/cli_server');
 const AsyncQueue = require('./src/async_queue');
 const parseRange = require('./src/parse_range');
@@ -995,56 +996,7 @@ async function init(queries, options) {
     });
     if (!audioFeeds || audioFeeds.err) return {meta, err: (audioFeeds || {}).err, code: 2};
 
-    var musicBrainz = [];
-    if (track.isrc !== "") {
-      trackLogger.print('| [\u2022] Obtaining MusicBrainz metadata...');
-  
-      const got = require('got');
-      var parser = require('xml2js');
-  
-      await got(`https://musicbrainz.org/ws/2/isrc/${track.isrc}?inc=artist-credits+releases`).then(response => {
-        trackLogger.write('[done]\n');
-        try {
-          // Should 'explicitArray: false' be used ?
-          parser.parseString(response.body, { trim: true, mergeAttrs: true }, function (err, result) {
-            const recording = result.metadata.isrc[0]['recording-list'][0]['recording'][0];
-  
-            try {
-              musicBrainz.trackId = recording['id'][0];
-            } catch { };
-            trackLogger.log(`| \u27a4 TrackId: ${musicBrainz.trackId}`);
-            try {
-              musicBrainz.artistId = recording['artist-credit'][0]['name-credit'][0]['artist'][0]['id'][0];
-            } catch { };
-            trackLogger.log(`| \u27a4 ArtistId: ${musicBrainz.artistId}`);
-  
-            // Searching for a matching album
-            const releases = recording['release-list'][0]['release'].filter(obj => {
-              const title = obj.title[0].replace(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"'); // Removing weird characters that can cause fails
-              return track.album.localeCompare(title) == 0;
-            });
-  
-            try {
-              musicBrainz.albumId = releases[0]['id'][0];
-              bim.hello = true;
-            }
-            catch { };
-            trackLogger.log(`| \u27a4 AlbumId: ${musicBrainz.albumId}`);
-            try {
-              musicBrainz.albumArtistId = releases[0]['artist-credit'][0]['name-credit'][0]['artist'][0]['id'][0];
-            } catch { };
-            trackLogger.log(`| \u27a4 AlbumArtistId: ${musicBrainz.albumArtistId}`);
-          });
-        } catch (error) {
-          trackLogger.log(error);
-        }
-        //
-      }).catch(error => {
-        trackLogger.write('[failed]\n');
-        trackLogger.log(error);
-      });
-    }
-    track.musicBrainz = musicBrainz;
+    track.musicBrainz = MusicBrainz.gatherMusicBrainzMetadata(track, trackLogger);
 
     const feedMeta = audioFeeds.formats.sort((meta1, meta2) => (meta1.abr > meta2.abr ? -1 : meta1.abr < meta2.abr ? 1 : 0))[0];
     meta.fingerprint = crypto
---
+++ src/musicbrainz.js
@@ -0,0 +1,58 @@
+async function gatherMusicBrainzMetadata(track, trackLogger)
+{
+	var musicBrainz = [];
+    if (track.isrc !== "") {
+      trackLogger.print('| [\u2022] Obtaining MusicBrainz metadata...');
+  
+      const got = require('got');
+      var parser = require('xml2js');
+  
+      await got(`https://musicbrainz.org/ws/2/isrc/${track.isrc}?inc=artist-credits+releases`).then(response => {
+        trackLogger.write('[done]\n');
+        try {
+          // Should 'explicitArray: false' be used ?
+          parser.parseString(response.body, { trim: true, mergeAttrs: true }, function (err, result) {
+            const recording = result.metadata.isrc[0]['recording-list'][0]['recording'][0];
+  
+            try {
+              musicBrainz.trackId = recording['id'][0];
+            } catch { };
+            trackLogger.log(`| \u27a4 TrackId: ${musicBrainz.trackId}`);
+            try {
+              musicBrainz.artistId = recording['artist-credit'][0]['name-credit'][0]['artist'][0]['id'][0];
+            } catch { };
+            trackLogger.log(`| \u27a4 ArtistId: ${musicBrainz.artistId}`);
+  
+            // Searching for a matching album
+            const releases = recording['release-list'][0]['release'].filter(obj => {
+              const title = obj.title[0].replace(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"'); // Removing weird characters that can cause fails
+              return track.album.localeCompare(title) == 0;
+            });
+  
+            try {
+              musicBrainz.albumId = releases[0]['id'][0];
+              bim.hello = true;
+            }
+            catch { };
+            trackLogger.log(`| \u27a4 AlbumId: ${musicBrainz.albumId}`);
+            try {
+              musicBrainz.albumArtistId = releases[0]['artist-credit'][0]['name-credit'][0]['artist'][0]['id'][0];
+            } catch { };
+            trackLogger.log(`| \u27a4 AlbumArtistId: ${musicBrainz.albumArtistId}`);
+          });
+        } catch (error) {
+          trackLogger.log(error);
+        }
+        //
+      }).catch(error => {
+        trackLogger.write('[failed]\n');
+        trackLogger.log(error);
+      });
+    }
+	return musicBrainz;
+}
+
+module.exports = {
+	gatherMusicBrainzMetadata,
+  };
+  
