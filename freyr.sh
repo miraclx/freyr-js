3a7d47b4b1e980a1606caeb77ec9874648bd5d3b
Miraculous Owonubi <omiraculous@gmail.com>
2022-08-11 13:03:55 +0400

    add shell pause when run from docker desktop

--- freyr.sh
+++ freyr.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
+if [ "$DOCKER_DESKTOP" == "true" ]; then
+  (
+    echo
+    echo
+    echo
+    echo "┌────────────────────────────────────────────────────────────┐"
+    echo "│      You are running freyr from inside Docker Desktop      │"
+    echo "│  Click on the CLI button at the top right to access a CLI  │"
+    echo "└────────────────────────────────────────────────────────────┘"
+  ) | (
+    COLS=$(tput cols)
+    while read -r line; do
+      printf "%*s" "$(((${COLS}-${#line})/2))";
+      echo "${line}"
+    done
+  )
+
+  tail -f /dev/null
+fi
+
 node "$(dirname "$0")"/cli.js "$@"
